7c223cda0d3af3e3e255fe603ffd865094d0d2eb
Max Zhou <ball908765@gmail.com>
2014-05-22 09:45:03 -0400

    made comparator and set up superclass

--- Player.java
+++ Player.java
@@ -1,5 +1,9 @@
-
+import java.*;
 
 public abstract class Player{
-    
+    public abstract int act(); //returns 1 if successful, 0 if otherwise
+    private int priority; //sort based on this
+    public int getPriority(){
+	return priority;
+    }
 }
