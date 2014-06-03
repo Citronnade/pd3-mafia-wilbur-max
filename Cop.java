039cef1c088080a906adbafd0c5b5d520b2b8dbe
Vincent Wong <vwong57@gmail.com>
2014-06-03 03:23:48 -0400

    cop class

--- Cop.java
+++ Cop.java
@@ -1,20 +1,31 @@
-  
+import java.util.*;
 
-Welcome to GNU Emacs, one component of the GNU/Linux operating system.
+public class Cop extends Player {
+    
+    public Cop (String name) {
+	this.name = name;
+	//not sure what else to put here
+	marks = new ArrayList <Mark> ();
+    }
+    
+    public int act() {
+	Player other;
+	other = chooseTarget();
+	if (false) {
+	    return 0;
+	}
+	else {
+	    inspect(other);
+	    return 0;
+	}
+    }
 
-Emacs Tutorial	Learn basic keystroke commands
-Emacs Guided Tour	Overview of Emacs features at gnu.org
-View Emacs Manual	View the Emacs manual using Info
-Absence of Warranty	GNU Emacs comes with ABSOLUTELY NO WARRANTY
-Copying Conditions	Conditions for redistributing and changing Emacs
-Ordering Manuals	Purchasing printed copies of manuals
+    public Player chooseTarget() {
+	
+    }
+    
+    public int inspect (Player other) {
+	//if player is guilty, return guilty else return innocent
+    }
 
-To start...     Open a File     Open Home Directory     Customize Startup
-To quit a partially entered command, type Control-g.
-
-This is GNU Emacs 23.3.1 (i686-pc-linux-gnu, GTK+ Version 2.24.10)
- of 2012-09-21 on akateko, modified by Debian
-Copyright (C) 2011 Free Software Foundation, Inc.
-
-If an Emacs session crashed recently, type Meta-x recover-session RET
-to recover the files you were editing.
+}
