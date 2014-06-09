a8026c0aa11fe40ea20b12663fb0337c5ec2a861
Vincent Wong <vwong57@gmail.com>
2014-06-09 01:17:05 -0400

    added bomb class

---
+++ Bomb.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Bomb extends Player{
+
+    //should we use static initialization blocks for this kind of stuff?
+    public Bomb(String name){
+	this.name = name;
+	marks = new ArrayList<Mark>();
+    }
+
+    public int act(){
+	return -1;
+    }
+
+    public int act(Player other){
+	return -1;
+    }
