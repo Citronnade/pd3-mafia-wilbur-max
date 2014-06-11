198fdd2417411d9a09f2057e063e4b4ecf0cdeb5
Vincent Wong <vwong57@gmail.com>
2014-06-10 23:36:36 -0400

    added Godfather class

---
+++ Godfather.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Godfather extends Mafia{
+
+    //should we use static initialization blocks for this kind of stuff?
+    public Godfather(String name){
+	this.name = name;
+	priority = 1;
+	marks = new ArrayList<Mark>();
+    }
+
+    public int act(){
+	return 0;
+    }
+
+    public int act(Player other){
+	if (false){
+	    return 0;
+	}
+	else{
+	    kill(other);
+	    return 1;
+	}
+	
+    }
+
+    public int kill(Player other){ //this can return some interesting information
+	for (Mark mark: marks){
+	    if (mark.getType() == "block"){
+		return -1; //mafia are weird since they have meetings.  But this'll work.
+	    }
+	}
+	other.addMark(new Mark(this, "death"));
+	return 1;
+    }
+}
